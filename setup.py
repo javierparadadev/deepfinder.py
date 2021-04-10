fc5a7f12d7e077b256209809fd07b2afc6fd21cb
Javier Parada <javierparada73@outlook.com>
2021-04-10 11:32:14 +0200

    Add more documentation.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='deepgetter',
     packages=find_packages(),
     version='0.0.0',
     description='',
     author='Javier Parada',
+    author_email="javierparada@protonmail.com",
     license='MIT',
+    url="https://github.com/parada3desu/deepgetter.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "deepgetter"},
+    python_requires=">=3.6",
 )
