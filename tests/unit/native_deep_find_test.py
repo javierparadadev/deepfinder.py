8fcab16b3c31edebe24875b1820ec76b4b7d4304
Javier Parada <javierparada73@outlook.com>
2021-10-09 14:00:22 +0200

    Fix tests.

--- tests/unit/native_deep_find_test.py
+++ tests/unit/native_deep_find_test.py
@@ -1,17 +1,15 @@
 import unittest
 
-from deepfinder.nativify import nativify
+from deepfinder.nativify import nativify, DeepFinderList, DeepFinderDict
 
 
 class TestNativeDeepFind(unittest.TestCase):
-
-    nativify()
 
     def test_first_value_of_a_list(self):
         """
         Should run native deepFind in list.
         """
-        data: [str] = list(['a', 'b', 'c'])
+        data: [str] = DeepFinderList(['a', 'b', 'c'])
         result = data.deep_find('0')
         self.assertEqual(result, 'a')
 
@@ -19,7 +17,7 @@ class TestNativeDeepFind(unittest.TestCase):
         """
         Should run native deepFind in dict.
         """
-        data: [str] = dict({'a': 'b', 'c': 'd'})
+        data: [str] = DeepFinderDict({'a': 'b', 'c': 'd'})
         result = data.deep_find('a')
         self.assertEqual(result, 'b')
 
