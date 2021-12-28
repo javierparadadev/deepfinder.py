4e2d3528ea1e955e2784666a7fce2a202daadf0c
Javier Parada <javierparada73@outlook.com>
2021-12-28 08:56:58 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='deepfinder',
-    version='0.0.8',
+    version='0.1.0',
     description='Search attributes easily within structures of type dictionary, list and embedded substructures with '
                 'simple format "dict.users.0.name".',
     author='Javier Parada',
